cc0fe56497a429bf6b1157b5f79ceadd668820e3
Jack Hughes <jhughes@openxtra.co.uk>
2014-06-30 17:28:15 +0100

    Moves the GetDataSourceByNameQuery into the Query namespace.

--- OrigoDbTest/Program.cs
+++ OrigoDbTest/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using OrigoDB.Core;
 using OrigoDB.Core.Test;
 using OrigoDbTest.Commands;
@@ -21,8 +20,10 @@ namespace OrigoDbTest
 
             theEngine.Close();
 
-            // Retrieve the data
+            // Re-load the database
             theEngine = Engine.Load<DataSourceModel>(config);
+
+            // Retrieve the data...
             var dataSourceCount = theEngine.Execute(new GetDataSourceCountQuery());
 
             Console.WriteLine("Found {0} data sources.", dataSourceCount);
@@ -31,29 +32,14 @@ namespace OrigoDbTest
             var dataSource = theEngine.Execute(new GetDataSourceByNameQuery(dataSourceToFind));
 
             if (dataSource != null)
-                Console.WriteLine("Found {0} data source.", dataSource.Name);
+                Console.WriteLine("Found '{0}' data source.", dataSource.Name);
             else
-                Console.WriteLine("Unable to find {0}.", dataSourceToFind);
+                Console.WriteLine("Unable to find '{0}'.", dataSourceToFind);
 
             Console.WriteLine("Press [Enter] to quit.");
             Console.ReadLine();
 
             theEngine.Close();
-        }
-    }
-
-    class GetDataSourceByNameQuery : Query<DataSourceModel, DataSource>
-    {
-        private readonly string dataSourceName;
-
-        public GetDataSourceByNameQuery(string theDataSourceName)
-        {
-            this.dataSourceName = theDataSourceName;
-        }
-
-        public override DataSource Execute(DataSourceModel model)
-        {
-            return model.DataSources.FirstOrDefault(ds => ds.Name == this.dataSourceName);
         }
     }
 }
---
+++ OrigoDbTest/Query/GetDataSourceByNameQuery.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using OrigoDB.Core;
+using OrigoDbTest.Models;
+
+namespace OrigoDbTest.Query
+{
+    class GetDataSourceByNameQuery : Query<DataSourceModel, DataSource>
+    {
+        private readonly string dataSourceName;
+
+        public GetDataSourceByNameQuery(string theDataSourceName)
+        {
+            this.dataSourceName = theDataSourceName;
+        }
+
+        public override DataSource Execute(DataSourceModel model)
+        {
+            return model.DataSources.FirstOrDefault(ds => ds.Name == this.dataSourceName);
+        }
+    }
+}
